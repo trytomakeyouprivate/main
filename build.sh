27b2d5f06f999bec65062600a22aff9a51d6c3cd
solidc0re <140627847+solidc0re@users.noreply.github.com>
2023-07-30 10:35:55 +0100

    Update build.sh
    added hardened kernel and malloc to solidblue and kinsolid

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:solidcore:rpm-kernel-hardened rpm-hardened-malloc
+
 rpm-ostree install \
     /tmp/rpms/*.rpm \
     fedora-repos-archive
