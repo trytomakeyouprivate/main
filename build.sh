524ad3de5671956ed168b8a3cd1ec0a2afb9f0af
solidc0re <140627847+solidc0re@users.noreply.github.com>
2023-07-30 11:32:55 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:solidcore:coreos-kernel-hardened rpm-kernel-hardened rpm-hardened-malloc
+wget https://copr.fedorainfracloud.org/coprs/solidcore/coreos-kernel-hardened/repo/fedora-$(RELEASE)/solidcore-coreos-kernel-hardened-fedora-$(RELEASE).repo -O /etc/yum.repos.d/copr_solidcore-kernel-hardened.repo
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:solidcore:coreos-kernel-hardened rpm-kernel-hardened
+rpm-ostree install rpm-hardened-malloc
 
 rpm-ostree install \
     /tmp/rpms/*.rpm \
